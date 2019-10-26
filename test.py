108b707e3124c4fb333e051729583c9888c3c311
1308215843 <1308215843@qq.com>
2019-10-26 11:01:54 +0800

    first one commit

---
+++ test.py
@@ -0,0 +1,30 @@
+# import time
+#
+# #20190223.1830
+#
+# min_hour = 6
+#
+# tss1 = "20190311.0930"
+# timeArray = time.strptime(tss1, "%Y%m%d.%H%M")
+# print(timeArray)
+#
+# timeStamp = int(time.mktime(timeArray))
+# print(timeStamp)
+#
+# print((time.time() - timeStamp)/3600)
+# if (time.time() - timeStamp)/3600 > min_hour:
+#     print("超过"+str(min_hour)+"小时")
+# else:
+#     print("未超过" + str(min_hour) + "小时")
+
+
+
+a = ""
+
+
+
+print(a)
+if not a:
+    print("1")
+else:
+    print("2")
